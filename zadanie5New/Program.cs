765355a8fb6b15b7eca5c3474e78880e2e85dfc6
Edi0501 <edytanowakowska05@gmail.com>
2024-02-18 08:40:12 +0100

    Add project files.

---
+++ zadanie5New/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Zadanie5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Podaj swój wiek");
+          //  Console.ReadLine();
+            Console.WriteLine("Podaj wiek:");
+            int age = int.Parse(Console.ReadLine());
+            if (age >= 21 && age < 30)
+            {
+                Console.WriteLine("Możesz zostać posłem");
+            }
+            else if (age >= 30 && age < 35)
+            {
+                Console.WriteLine("Możesz zostać posłem lub senatorem");
+            }
+            else if (age >= 35)
+            {
+                Console.WriteLine("Możesz zostać posłem, senatorem lub prezydentem");
+            }
+            else
+            {
+                Console.WriteLine("Nie możesz kandydować");
+            }
+            
+        }
+    }
+}
